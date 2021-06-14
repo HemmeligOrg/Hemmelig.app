d617f1e08d184a432373f24c3878b5d9c04a2506
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-06-14 08:42:58 +0200

    Fix typos for the privacy page

--- src/client/routes/privacy/index.js
+++ src/client/routes/privacy/index.js
@@ -12,20 +12,20 @@ const Privacy = () => {
                 <h2>Is my data secure?</h2>
                 <Info align="left">
                     Yes, your data is secure. Hemmelig is encrypting every message with the AES 256
-                    algoritm with a dynamic IV before saving it to our database with a master
+                    algorithm with a dynamic IV before saving it to our database with a master
                     password.
                 </Info>
 
                 <h2>Do you track me?</h2>
                 <Info align="left">
-                    We do not track anything. Hemmelig care strongly about your privacy. Future wise
-                    we might track the status of how many secrets that is being created, which is
-                    not personal data, and will be publicly available on this site.
+                    We do not track anything. Hemmelig cares strongly about your privacy. Future
+                    wise we might track the status of how many secrets that are being created, which
+                    is not personal data, and will be publicly available on this site.
                 </Info>
 
                 <h2>I still don't trust this application.</h2>
                 <Info align="left">
-                    If that is being the case, Hemmelig offers a docker image to self host the
+                    If that is being the case, Hemmelig offers a docker image to self-host the
                     application.
                 </Info>
             </Wrapper>
