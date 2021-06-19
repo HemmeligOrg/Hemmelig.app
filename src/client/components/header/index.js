aa5ab7c9d30152cf1869ff27e55777ee870c6738
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-06-19 10:24:26 +0200

    Align the header with logo

--- src/client/components/header/index.js
+++ src/client/components/header/index.js
@@ -5,7 +5,9 @@ import style from './style.css';
 const Header = () => (
     <header class={style.header}>
         <Link class={style.link} href="/">
-            <Logo class={style.logo} />
+            <div class={style.logoWrapper}>
+                <Logo class={style.logo} /> <span class={style.logoText}>hemmelig</span>
+            </div>
         </Link>
     </header>
 );
