cd261208256bd70ba78757a00ce5563697396220
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-06-24 08:47:21 +0200

    Update crypto test

--- tests/server/helpers/crypto.test.js
+++ tests/server/helpers/crypto.test.js
@@ -4,15 +4,17 @@ const { encrypt, decrypt } = require('../../../src/server/helpers/crypto');
 
 const SECRET = 'MASTER_KEY=1337-super-secret';
 
+const RANDOM_KEY = '321312312312312312312312312';
+
 describe('Crypto', () => {
     describe('#encrypt(string)', () => {
         it('should encrypt, and decrypt a secret', async () => {
-            const encrypted = await encrypt(SECRET);
+            const encrypted = await encrypt(SECRET, RANDOM_KEY);
 
             assert.equal('iv' in encrypted, true);
             assert.equal('content' in encrypted, true);
 
-            const decrypted = await decrypt(encrypted);
+            const decrypted = await decrypt(encrypted, RANDOM_KEY);
 
             assert.equal(decrypted, SECRET);
         });
