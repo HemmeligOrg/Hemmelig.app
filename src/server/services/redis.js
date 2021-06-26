bf99f52fe8fb8a47252f5d07076b0dbdf4eaa377
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-06-26 19:54:39 +0200

    Check if key is in data

--- src/server/services/redis.js
+++ src/server/services/redis.js
@@ -53,7 +53,7 @@ async function getSecret(id) {
 async function getSecretKey(id, key) {
     const data = await client.hgetall(`secret:${id}`);
 
-    if (data[key]) {
+    if (key in data) {
         return data[key];
     }
 
