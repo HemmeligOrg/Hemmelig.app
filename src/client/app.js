02babc3df279582c21f1bef45fb16d1dadcad69b
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-08-18 23:09:07 +0200

    fix: remove sonar code smells

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
     MantineProvider,
     AppShell,
