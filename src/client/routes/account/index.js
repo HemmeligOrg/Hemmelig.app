ed99d48f1dd9a243bca921131dbe4c504292ec10
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-08-21 21:14:44 +0200

    chore: remove copy and pasted text

--- src/client/routes/account/index.js
+++ src/client/routes/account/index.js
@@ -83,12 +83,7 @@ const Account = () => {
         openConfirmModal({
             title: 'Delete your profile',
             centered: true,
-            children: (
-                <Text size="sm">
-                    Are you sure you want to delete your profile? This action is destructive and you
-                    will have to contact support to restore your data.
-                </Text>
-            ),
+            children: <Text size="sm">Are you sure you want to delete your profile?</Text>,
             labels: { confirm: 'Delete account', cancel: "No don't delete it" },
             confirmProps: { color: 'red' },
             onConfirm: () => onDeleteUser(),
