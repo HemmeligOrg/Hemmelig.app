a6cb9aa75a4f427ad284bdf065198513d3a5ac52
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-08-29 22:41:29 +0200

    chore: change the stats route

--- src/server/controllers/stats.js
+++ src/server/controllers/stats.js
@@ -1,7 +1,7 @@
 import * as redis from '../services/redis.js';
 
 async function statistics(fastify) {
-    fastify.get('/', async (_, reply) => {
+    fastify.get('/secrets_created', async (_, reply) => {
         const statistics = await redis.getStatistics('secrets_created');
 
         return reply.code(200).send(statistics);
