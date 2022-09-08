d9da3a424e511e3e47754fa8d6482de5d479a689
RainerZufahl <crafter45@gmail.com>
2022-09-08 15:05:10 +0200

    added LanguageDetector
    used for browser language detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,13 @@
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
 
 i18next
     .use(initReactI18next)
     .use(HttpApi)
+    .use(LanguageDetector)
     .init({
-        // Remove resources from here
-        lng: 'en',
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false,
