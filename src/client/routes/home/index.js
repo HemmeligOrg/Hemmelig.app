cea26a637c8acbf77f1f9864a18d36ce502f783a
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-09-13 20:51:00 +0200

    chore: fix code smells

--- src/client/routes/home/index.js
+++ src/client/routes/home/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import passwordGenerator from 'generate-password';
 import {
     Button,
