06dcbfe371e63ab421ebeea5409d6a7f69b9760a
bjarneo <bjarne.oeverli@gmail.com>
2023-04-09 14:57:24 +0200

    fix: rename the zip file

--- src/client/api/upload.js
+++ src/client/api/upload.js
@@ -32,7 +32,7 @@ export const downloadFile = async (fileData) => {
     const blob = new Blob([fileContent], { type });
     const url = window.URL.createObjectURL(blob);
     a.href = url;
-    a.download = key + ext;
+    a.download = 'hemmelig_files' + ext;
     a.click();
     window.URL.revokeObjectURL(url);
 };
