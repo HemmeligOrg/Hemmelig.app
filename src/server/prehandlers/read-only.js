fd823eb867eed9c8cda14c85b1dd1b074e19c3d5
bjarneo <bjarne.oeverli@gmail.com>
2023-04-09 09:03:17 +0200

    refactor: remove the cookie token handling from the handler

--- src/server/prehandlers/read-only.js
+++ src/server/prehandlers/read-only.js
@@ -2,34 +2,29 @@ import prisma from '../services/prisma.js';
 import adminSettings from '../adminSettings.js';
 
 // The auth routes
-const regex = /^\/api\/authentication\/.*$/i;
+const authRegex = /^\/api\/authentication\/.*$/i;
+const accountRegex = /^\/api\/account\/.*$/i;
 
-export default function readOnlyHandler(fastify) {
-    return async (request, reply) => {
-        const { url } = request;
+export default async function readOnlyHandler(request, reply) {
+    const { url } = request;
 
-        if (regex.test(url)) {
-            return;
-        }
+    if (authRegex.test(url) || accountRegex.test(url)) {
+        return;
+    }
 
-        const token = request.cookies['__HEMMELIG_TOKEN'] || null;
+    const username = request?.user?.username ?? null;
 
-        if (!token && adminSettings.get('read_only')) {
+    if (!username && adminSettings.get('read_only')) {
+        return reply.code(403).send({ error: 'Access denied' });
+    }
+
+    if (username) {
+        const user = await prisma.user.findFirst({
+            where: { username },
+        });
+
+        if (user.role !== 'admin' && adminSettings.get('read_only')) {
             return reply.code(403).send({ error: 'Access denied' });
         }
-
-        if (token) {
-            const decoded = fastify.jwt.verify(token);
-
-            const username = decoded?.username || null;
-
-            const user = await prisma.user.findFirst({
-                where: { username },
-            });
-
-            if (user.role !== 'admin' && adminSettings.get('read_only')) {
-                return reply.code(403).send({ error: 'Access denied' });
-            }
-        }
-    };
+    }
 }
