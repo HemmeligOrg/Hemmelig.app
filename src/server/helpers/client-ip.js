33d9218a18e9fb6b7bfc6732a1baf38a9b403168
bjarneo <bjarne.oeverli@gmail.com>
2023-05-06 16:26:32 +0200

    fix: add cloudflare connecting ip to the list
    also rewrite the code to support if the x-forwarded-for has multiple ips, return the first

--- src/server/helpers/client-ip.js
+++ src/server/helpers/client-ip.js
@@ -3,8 +3,9 @@ import config from 'config';
 export default function getClientIp(headers) {
     // Iterate through a list of headers allowed to fetch the ip from
     const checkHeaders = [
+        'cf-connecting-ip', // cloudflare
         'do-connecting-ip', // digital ocean app platform
-        'true-client-ip', // used for cloudflare and cloudfront
+        'true-client-ip', // used for cloudfront
         'x-real-ip',
         'x-forwarded-for',
     ];
@@ -15,6 +16,13 @@ export default function getClientIp(headers) {
 
     const header = checkHeaders.find((header) => headers[header]);
 
+    const ip = headers[header] ? headers[header] : '';
+
+    if (ip && ip.includes(',')) {
+        // return the first ip in the list
+        return ip.split(',')[0];
+    }
+
     // return the first existing header
-    return headers[header] ? headers[header] : '';
+    return ip;
 }
