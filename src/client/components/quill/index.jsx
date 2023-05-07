bc7fb715797e2094fc1f1adfdd78a15a3427cb77
bjarneo <bjarne.oeverli@gmail.com>
2023-05-07 12:46:00 +0200

    chore: remove indent and add code block

--- src/client/components/quill/index.jsx
+++ src/client/components/quill/index.jsx
@@ -5,8 +5,8 @@ const modules = {
     toolbar: [
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-        ['link', 'image'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['link', 'image', 'code-block'],
     ],
 };
 
@@ -19,9 +19,9 @@ const formats = [
     'blockquote',
     'list',
     'bullet',
-    'indent',
     'link',
     'image',
+    'code-block',
 ];
 
 export default function Quill(props) {
