da1d9b10dab9912bd1bb0501148f3fc0b314a3aa
bjarneo <bjarne.oeverli@gmail.com>
2023-07-21 07:41:47 +0200

    chore: add error logging and throw an error for the password creation / compare

--- src/server/helpers/password.js
+++ src/server/helpers/password.js
@@ -5,15 +5,19 @@ export async function hash(password) {
     try {
         // salt rounds https://www.npmjs.com/package/bcrypt#user-content-a-note-on-rounds
         return await bcrypt.hash(password, 10);
-    } catch (err) {
-        console.error(err);
+    } catch (error) {
+        console.error(error);
+
+        throw new Error('Error hashing the password');
     }
 }
 
 export async function compare(password, hash) {
     try {
         return await bcrypt.compare(password, hash);
-    } catch (_) {
+    } catch (error) {
+        console.error(error);
+
         return false;
     }
 }
