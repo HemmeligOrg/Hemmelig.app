d9aa7af4c9b448ff682b7a8c8dba8930c469d8fe
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-13 15:39:18 -0800

    chore: manually change things missed by codemod

--- src/server/services/do.js
+++ src/server/services/do.js
@@ -1,27 +1,11 @@
-import AWS from 'aws-sdk';
 import { Upload } from '@aws-sdk/lib-storage';
 import { S3 } from '@aws-sdk/client-s3';
 import { nanoid } from 'nanoid';
 import config from 'config';
 
-// Set the Region
-// JS SDK v3 does not support global configuration.
-// Codemod has attempted to pass values to each service client in this file.
-// You may need to update clients outside of this file, if they use global config.
-AWS.config.update({
-    region: 'hemmelig',
-    accessKeyId: config.get('do.spaces.key'),
-    secretAccessKey: config.get('do.spaces.secret'),
-});
-
 const s3 = new S3({
-    // The transformation for endpoint is not implemented.
-    // Refer to UPGRADING.md on aws-sdk-js-v3 for changes needed.
-    // Please create/upvote feature request on aws-sdk-js-codemod for endpoint.
-    endpoint: new AWS.Endpoint(config.get('do.spaces.endpoint')),
-
+    endpoint: config.get('do.spaces.endpoint'),
     region: 'hemmelig',
-
     credentials: {
         accessKeyId: config.get('do.spaces.key'),
         secretAccessKey: config.get('do.spaces.secret'),
@@ -34,7 +18,6 @@ export async function upload(fileUpload) {
     try {
         await new Upload({
             client: s3,
-
             params: {
                 Bucket: config.get('do.spaces.bucket'),
                 Key: `${config.get('do.spaces.folder')}/${filename}.json`,
