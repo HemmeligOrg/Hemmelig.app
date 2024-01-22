c3af811d0479884dcbf8000953c5aa34bfc83c4b
bjarneo <bjarne.oeverli@gmail.com>
2024-01-22 18:14:37 +0100

    feat: add totalt active secrets to the stats
    also, change the route

--- src/server/controllers/stats.js
+++ src/server/controllers/stats.js
@@ -1,10 +1,16 @@
 import prisma from '../services/prisma.js';
 
 async function statistics(fastify) {
-    fastify.get('/secrets_created', async (_, reply) => {
-        const statistics = await prisma.statistic.findMany();
+    fastify.get('/', async (_, reply) => {
+        const [totalSecretsCreated, activeSecrets] = await Promise.all([
+            prisma.statistic.findMany(),
+            prisma.secret.count(),
+        ]);
 
-        return reply.code(200).send(statistics);
+        const [total = { value: 0 }] = totalSecretsCreated;
+        const { value } = total;
+
+        return reply.code(200).send({ totalSecretsCreated: value, activeSecrets });
     });
 }
 
