d03608e5cecb9bb32953b10e5398b0fa31619a19
bjarneo <bjarne.oeverli@gmail.com>
2024-01-24 16:52:02 +0100

    fix: remove the backend config

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 import config from './client/config';
@@ -20,7 +19,6 @@ const { language, detectionMethod } = getLanguage();
 
 i18next
     .use(initReactI18next)
-    .use(HttpApi)
     .use(LanguageDetector)
     .init({
         fallbackLng: language,
