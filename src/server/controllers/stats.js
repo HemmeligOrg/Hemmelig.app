d1409a7ce5320109d55629c50ad2cb93137f5caf
bjarneo <bjarne.oeverli@gmail.com>
2024-01-27 15:55:24 +0100

    fix: add public secrets stats

--- src/server/controllers/stats.js
+++ src/server/controllers/stats.js
@@ -2,15 +2,16 @@ import prisma from '../services/prisma.js';
 
 async function statistics(fastify) {
     fastify.get('/', async (_, reply) => {
-        const [totalSecretsCreated, activeSecrets] = await Promise.all([
+        const [totalSecretsCreated, activeSecrets, isPublicSecrets] = await Promise.all([
             prisma.statistic.findMany(),
             prisma.secret.count(),
+            prisma.secret.count({ where: { isPublic: true } }),
         ]);
 
         const [total = { value: 0 }] = totalSecretsCreated;
         const { value } = total;
 
-        return reply.code(200).send({ totalSecretsCreated: value, activeSecrets });
+        return reply.code(200).send({ totalSecretsCreated: value, activeSecrets, isPublicSecrets });
     });
 }
 
