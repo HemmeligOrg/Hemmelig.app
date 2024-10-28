9fb407b2520ada4c9760752355e94b3a8515d55a
bjarneo <bjarne.oeverli@gmail.com>
2024-10-28 10:29:11 +0100

    initial

--- client/components/quill/index.jsx
+++ client/components/quill/index.jsx
@@ -2,6 +2,21 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const Quill = ({ value, onChange, readOnly, defaultValue }) => {
+    // Define modules based on readOnly state
+    const modules = readOnly
+        ? {
+              toolbar: false, // Disable toolbar in read-only mode
+          }
+        : {
+              toolbar: [
+                  [{ header: [1, 2, 3, false] }],
+                  ['bold', 'italic', 'underline', 'strike'],
+                  [{ list: 'ordered' }, { list: 'bullet' }],
+                  ['link', 'code-block'],
+                  ['clean'],
+              ],
+          };
+
     return (
         <div className="bg-gray-800 border border-gray-700 rounded-md overflow-hidden">
             <ReactQuill
@@ -9,16 +24,8 @@ const Quill = ({ value, onChange, readOnly, defaultValue }) => {
                 onChange={onChange}
                 readOnly={readOnly}
                 placeholder={defaultValue}
-                theme="snow"
-                modules={{
-                    toolbar: [
-                        [{ header: [1, 2, 3, false] }],
-                        ['bold', 'italic', 'underline', 'strike'],
-                        [{ list: 'ordered' }, { list: 'bullet' }],
-                        ['link', 'code-block'],
-                        ['clean'],
-                    ],
-                }}
+                theme={readOnly ? 'bubble' : 'snow'}
+                modules={modules}
                 className="bg-gray-800 text-gray-100 placeholder-gray-500"
             />
         </div>
