ae033576a83485eb100e498cc5b4d1acaf7e395c
bjarneo <bjarne.oeverli@gmail.com>
2024-10-28 11:01:27 +0100

    initial

--- client/admin-shell.jsx
+++ client/admin-shell.jsx
@@ -1,4 +1,5 @@
-import { Anchor, AppShell, Group, Header, Navbar, useMantineTheme } from '@mantine/core';
+import { IconMenu2 } from '@tabler/icons';
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import Logo from './components/header/logo';
@@ -8,54 +9,70 @@ import MainLinks from './components/settings/main-links';
 import SecondaryLinks from './components/settings/secondary-links';
 
 const AdminShell = () => {
-    const theme = useMantineTheme();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     return (
-        <AppShell
-            padding="md"
-            fixed={false}
-            navbar={
-                <Navbar
-                    width={{ base: 300 }}
-                    height={500}
-                    p="xs"
-                    sx={(theme) => ({
-                        backgroundColor: theme.colors.dark[8],
-                    })}
-                >
-                    <Navbar.Section grow mt="xs">
-                        <MainLinks />
-                    </Navbar.Section>
-                    <Navbar.Section grow mt="xs">
-                        <SecondaryLinks />
-                    </Navbar.Section>
-                </Navbar>
-            }
-            header={
-                <Header
-                    height={75}
-                    style={{ zIndex: 1100 }}
-                    sx={(theme) => ({
-                        backgroundColor: theme.colors.dark[8],
-                    })}
-                >
-                    <Group sx={{ height: '100%', paddingTop: '5px' }} px={20} position="apart">
-                        <Anchor component={Link} to="/">
-                            <Logo className={logoStyles.logo} />
-                        </Anchor>
+        <div className="min-h-screen bg-gray-900">
+            {/* Header */}
+            <header className="fixed top-0 left-0 right-0 h-[75px] bg-gray-800 z-50">
+                <div className="h-full px-5 flex items-center justify-between">
+                    {/* Logo */}
+                    <Link to="/" className="flex items-center">
+                        <Logo className={logoStyles.logo} />
+                    </Link>
 
+                    {/* Mobile Menu Button */}
+                    <button
+                        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                        className="lg:hidden p-2 rounded-md text-gray-400 hover:text-white 
+                                 hover:bg-gray-700 focus:outline-none focus:ring-2 
+                                 focus:ring-gray-600"
+                    >
+                        <IconMenu2 size={24} />
+                    </button>
+
+                    {/* Navigation */}
+                    <div className="hidden lg:block">
                         <Nav />
-                    </Group>
-                </Header>
-            }
-            styles={{
-                main: {
-                    background: theme.colors.dark[8],
-                },
-            }}
-        >
-            <Outlet />
-        </AppShell>
+                    </div>
+                </div>
+            </header>
+
+            {/* Sidebar */}
+            <aside
+                className={`fixed top-[75px] left-0 h-[calc(100vh-75px)] w-[300px] bg-gray-800 
+                           transform transition-transform duration-300 ease-in-out lg:translate-x-0
+                           ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
+            >
+                <div className="h-full flex flex-col p-4">
+                    {/* Main Links Section */}
+                    <div className="flex-1">
+                        <MainLinks />
+                    </div>
+
+                    {/* Secondary Links Section */}
+                    <div className="flex-1">
+                        <SecondaryLinks />
+                    </div>
+                </div>
+            </aside>
+
+            {/* Main Content */}
+            <main
+                className={`min-h-[calc(100vh-75px)] transition-all duration-300 ease-in-out
+                           lg:ml-[300px] mt-[75px] p-6`}
+            >
+                <Outlet />
+            </main>
+
+            {/* Overlay for mobile sidebar */}
+            {isSidebarOpen && (
+                <div
+                    className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
+                    onClick={() => setIsSidebarOpen(false)}
+                />
+            )}
+        </div>
     );
 };
 
