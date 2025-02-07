e9da86edb352b71b6f2f1fc3f0a5336c173bc9be
bjarneo <bjarneo@users.noreply.github.com>
2025-02-07 15:01:36 +0100

    Revert "chore: set the git tag version and sha (#357)" (#358)
    This reverts commit 0f6e77cfaaf0f32dc5ddcef5302937f02d2e69b8.

--- client/config/default.js
+++ client/config/default.js
@@ -5,8 +5,4 @@ export default {
     settings: {
         forcedLanguage: '',
     },
-    git: {
-        sha: import.meta.env.VITE_GIT_SHA || 'development',
-        tag: import.meta.env.VITE_GIT_TAG || 'development',
-    },
 };
