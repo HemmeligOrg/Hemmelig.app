84e8b30751ba06389003de8cef07af2b1b95b01f
bjarneo <bjarne.oeverli@gmail.com>
2025-02-25 10:08:08 +0100

    chore: remove test content

--- client/components/editor/index.jsx
+++ client/components/editor/index.jsx
@@ -616,37 +616,6 @@ const extensions = [
     }),
 ];
 
-const content = `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you'd probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That's a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn't that great? And all of that is editable. But wait, there's more. Let's try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It's only the tip of the iceberg though. Give it a try and click a little bit around. Don't forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that's amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
-`;
-
 export default function Editor({ content = '', setContent, editable = true }) {
     return (
         <div className="prose prose-sm max-w-none border border-gray-700 rounded-md p-2 min-h-72">
