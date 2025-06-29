b88bc0d00d74a53f3357b2cdf50d6f5d1df9e5d0
Bjarne <bjarne.oeverli@gmail.com>
2025-06-29 11:15:25 +0200

    fix: load tabler icons properly, and not all 5k plus components

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,10 @@ export default defineConfig({
     optimizeDeps: {
         exclude: ['lucide-react'],
     },
+    resolve: {
+        alias: {
+            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+            '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+        },
+    },
 });
