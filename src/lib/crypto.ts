335a04dad80994b3dd45b9c5153216de3587f49f
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-11-19 16:47:28 +0100

    Revert "Change the nvim name to aether"
    This reverts commit eff23b2a404c3f51350ec403a520cb777c436c7f.

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,11 +1,11 @@
 import { nanoid } from 'nanoid';
 
 /**
- * returns the user-provided password or generates a random 32-character string to be used as an encryption key.
- * @param {string} [password] - the user's password. if not provided, a random key is generated.
- * @returns {string} - the password or the generated key.
+ * Returns the user-provided password or generates a random 32-character string to be used as an encryption key.
+ * @param {string} [password] - The user's password. If not provided, a random key is generated.
+ * @returns {string} - The password or the generated key.
  */
-export const generateencryptionkey = (password?: string): string => {
+export const generateEncryptionKey = (password?: string): string => {
     if (password && password.length > 0) {
         return password;
     }
@@ -13,10 +13,10 @@ export const generateencryptionkey = (password?: string): string => {
 };
 
 /**
- * generates a random 256-bit (32-byte) salt.
- * @returns {string} a 32-character string to be used as a salt.
+ * Generates a random 256-bit (32-byte) salt.
+ * @returns {string} A 32-character string to be used as a salt.
  */
-export const generatesalt = (): string => {
+export const generateSalt = (): string => {
     return nanoid(32);
 };
 
