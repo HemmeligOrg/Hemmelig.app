513d450798ea947e3be9efb305368aa0c6996517
Bjarne Øverli <bjarne.oeverli@gmail.com>
2025-12-02 19:25:00 +0100

    Fix the lucide react load

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         }),
     ],
     optimizeDeps: {
-        exclude: ['lucide-react'],
+        include: ['lucide-react'],
     },
     resolve: {
         alias: {
