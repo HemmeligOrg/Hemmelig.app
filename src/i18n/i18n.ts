e89545ea901ffd1eabf40c9a7d4f8125a26b704c
Bjarne Øverli <bjarne.oeverli@gmail.com>
2025-12-04 12:06:40 +0100

    Ensure spanish work

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,24 +1,27 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import enTranslations from './locales/en/en.json';
 import esTranslations from './locales/es/es.json';
 
-i18n.use(initReactI18next).init({
-    fallbackLng: 'en',
-    lng: 'en', // default language
-    detection: {
-        order: ['navigator'],
-    },
-    debug: true,
-    interpolation: {
-        escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-        en: { translations: enTranslations },
-        es: { translations: esTranslations },
-    },
-    defaultNS: 'translations',
-});
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        debug: true,
+        detection: {
+            order: ['navigator', 'htmlTag'],
+            caches: [],
+        },
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        resources: {
+            en: { translations: enTranslations },
+            es: { translations: esTranslations },
+        },
+        defaultNS: 'translations',
+    });
 
 export default i18n;
