d817cf6aafac51217ebd7b819b84fde96cfde174
Bjarne Øverli <bjarne.oeverli@gmail.com>
2025-12-04 21:00:40 +0100

    Use a tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,12 +24,12 @@ export function Footer() {
                             Terms & Conditions
                         </Link>
                         <span className="text-gray-400 dark:text-slate-500">|</span>
-                        <Link
-                            to="/api/docs"
+                        <a
+                            href="/api/docs"
                             className="text-gray-500 dark:text-slate-400 hover:text-gray-700 dark:hover:text-slate-200 text-xs transition-colors"
                         >
                             API Docs
-                        </Link>
+                        </a>
                     </div>
 
                     {/* Right - GitHub, X and Theme Toggle */}
