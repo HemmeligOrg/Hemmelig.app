5e8c5c7ac0896419991ba18222487ed1a7ffcb69
Diego <Baeth@users.noreply.github.com>
2026-05-17 10:41:24 -0300

    Add pt translation
    Adding pt-BR translation as a generic pt fow now, so both languages can benefit from it.
    
    Added nonExplicitSupportedLngs: true, so that breowsers with pt-br can fallback to pt.

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -10,6 +10,7 @@ import frTranslations from './locales/fr/fr.json';
 import itTranslations from './locales/it/it.json';
 import nlTranslations from './locales/nl/nl.json';
 import noTranslations from './locales/no/no.json';
+import ptTranslations from './locales/pt/pt.json';
 import svTranslations from './locales/sv/sv.json';
 import zhTranslations from './locales/zh/zh.json';
 
@@ -17,6 +18,7 @@ i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: 'en',
+        nonExplicitSupportedLngs: true,
         debug: true,
         detection: {
             order: ['localStorage', 'navigator', 'htmlTag'],
@@ -37,6 +39,7 @@ i18n.use(LanguageDetector)
             da: { translations: daTranslations },
             no: { translations: noTranslations },
             sv: { translations: svTranslations },
+            pt: { translations: ptTranslations },
         },
         defaultNS: 'translations',
     });
